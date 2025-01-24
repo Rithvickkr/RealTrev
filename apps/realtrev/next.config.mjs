66a3803676df7f9892de68063b080b674ff6aa77
Rithvickkr <12216061@nitkkr.ac.in>
2025-01-24 22:02:20 +0530

    Add ESLint configuration to ignore errors during builds

--- apps/realtrev/next.config.mjs
+++ apps/realtrev/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
       domains: ['plus.unsplash.com'], // Add allowed image domains here
     },
+    eslint: {
+      ignoreDuringBuilds: true,
+    }
   };
   
   export default nextConfig;
