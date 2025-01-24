3d18b6c09fb4c64cf6825b741aa6299ff807178b
Rithvickkr <12216061@nitkkr.ac.in>
2025-01-24 23:00:27 +0530

    Refactor setResponseQ function to use QueryStatus enum for improved readability and maintainability

--- apps/realtrev/app/lib/actions/setresponseq.ts
+++ apps/realtrev/app/lib/actions/setresponseq.ts
@@ -1,29 +1,34 @@
-"use server"
-import { QueryStatus } from "@prisma/client";
+"use server";
+
 import prisma from "@repo/db/client";
 
-export default async function setResponseQ(session: any,queryId: string) {
-    const response = await prisma.query.findUnique({
-        where: {
-        id: queryId,
-        },
-    });
-    if (response) {
-        if (response.status === QueryStatus.PENDING) {
-            await prisma.query.update({
-                where: {
-                    id: queryId,
-                },
-                data: {
-                    status: "ACCEPTED",
-                    responderId: session.user.id,
-                },
-            });
-            console.log("Query responded to");
-            return true;
-        }
-    }
-    console.log("Query not found or already responded to");
-    return false;
+enum QueryStatus {
+  PENDING = "PENDING",
+  ACCEPTED = "ACCEPTED",
+  REJECTED = "REJECTED",
 }
 
+export default async function setResponseQ(session: any, queryId: string) {
+  const response = await prisma.query.findUnique({
+    where: {
+      id: queryId,
+    },
+  });
+  if (response) {
+    if (response.status === QueryStatus.PENDING) {
+      await prisma.query.update({
+        where: {
+          id: queryId,
+        },
+        data: {
+          status: "ACCEPTED",
+          responderId: session.user.id,
+        },
+      });
+      console.log("Query responded to");
+      return true;
+    }
+  }
+  console.log("Query not found or already responded to");
+  return false;
+}
